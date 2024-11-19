93384ded6d08f554ecd72d3207a2b4d98e4754fa
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 14:10:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import altair as alt
 # Charger les données
 data = pd.read_csv('Iris.csv', delimiter=";") 
 #Créer un titre
-st.title("Mon premier tableau de bord Streamlit") 
+st.title("Dashboard Streamlit") 
 #Afficher les données dans un tableau
 #st.table(data)
 #Affichage des 5 premières lignes du jeu de doonnées
