2970f7aea0f13b0a3cb6bfc848439d9edac626bf
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 15:14:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,20 @@ chart = alt.Chart(data).mark_point().encode(x='SepalLength' , y='PetalLength')
 
 st.altair_chart(chart, use_container_width=True)
 
-# import streamlit as st
+import streamlit as st
 
-# # Title for the main app
-# st.title("Main App Area")
+# Title for the main app
+st.title("Main App Area")
 
-# # Title for the sidebar
-# st.sidebar.title("Sidebar")
+# Title for the sidebar
+st.sidebar.title("Sidebar")
 
-# # Add widgets to the sidebar
-# option = st.sidebar.selectbox(
-#     "Select an option:",
-#     ["Option iris", "Option data", "Option github"]
-# )
+# Add widgets to the sidebar
+option = st.sidebar.selectbox(
+    "Select an option:",
+    ["Option iris", "Option data", "Option github"]
+)
 
-# # Display selected option
-# st.write(f"You selected: {option}")
+# Display selected option
+st.write(f"You selected: {option}")
 
