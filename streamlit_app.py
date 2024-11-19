2f0582b57448b51e67c9560214c33c257fbc60ce
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 14:44:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,4 @@ st.altair_chart(chart, use_container_width=True)
 
 
 with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
-
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
-
+    st.title("visualisation")
