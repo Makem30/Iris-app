bed1126d20694e0a86883e133649a1d8a8a4f16c
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 13:45:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,15 @@ st.write(data.head())
 st.subheader("Aperçu des données")
 
 # Sélecteur d'espèce
-species = st.selectbox("Choisissez une espèce", iris['species'].unique())
+Species = st.selectbox("Choisissez une espèce", data['Species'].unique())
 
 # Filtrer les données selon l'espèce choisie
-filtered_data = data[data['species'] == species]
+filtered_data = data[data['Species'] == Species]
 
 # Graphique de distribution de la longueur des sépales
-st.subheader(f"Distribution de la longueur des sépales pour {species}")
+st.subheader(f"Distribution de la longueur des sépales pour {Species}")
 length_chart = alt.Chart(filtered_data).mark_bar().encode(
-    alt.X("sepal_length:Q", bin=alt.Bin(maxbins=20)),
+    alt.X("SepalLength:Q", bin=alt.Bin(maxbins=20)),
     alt.Y("count():Q"),
     tooltip=["sepal_length:Q", "count():Q"]
 ).properties(width=600, height=400)
@@ -30,22 +30,22 @@ length_chart = alt.Chart(filtered_data).mark_bar().encode(
 st.altair_chart(length_chart, use_container_width=True)
 
 # Graphique de distribution de la largeur des sépales
-st.subheader(f"Distribution de la largeur des sépales pour {species}")
+st.subheader(f"Distribution de la largeur des sépales pour {Species}")
 width_chart = alt.Chart(filtered_data).mark_bar().encode(
-    alt.X("sepal_width:Q", bin=alt.Bin(maxbins=20)),
+    alt.X("SepalWidth:Q", bin=alt.Bin(maxbins=20)),
     alt.Y("count():Q"),
-    tooltip=["sepal_width:Q", "count():Q"]
+    tooltip=["SepalWidth:Q", "count():Q"]
 ).properties(width=600, height=400)
 
 st.altair_chart(width_chart, use_container_width=True)
 
 # Graphique de dispersion
-st.subheader(f"Graphique de dispersion pour {species}")
+st.subheader(f"Graphique de dispersion pour {Species}")
 scatter_chart = alt.Chart(filtered_data).mark_circle(size=60).encode(
-    x='sepal_length:Q',
-    y='sepal_width:Q',
-    color='species:N',
-    tooltip=['sepal_length', 'sepal_width', 'species']
+    x='SepalLength:Q',
+    y='SepalWidth:Q',
+    color='Species:N',
+    tooltip=['SepalLength', 'SepalWidth', 'Species']
 ).properties(width=600, height=400)
 
 st.altair_chart(scatter_chart, use_container_width=True)
