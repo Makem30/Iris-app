f62dce6a03047ed529d69b2011b91184940d75f7
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 11:20:47 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-
+import streamlit as st
+import pandas as pd 
+# Charger les données
+data = pd.read_csv('Iris.csv') 
+#Créer un titre
+st.title("Mon premier tableau de bord Streamlit") 
+#Afficher les données dans un tableau
+st.table(data)
