9164b759339ae4138e8c790f7c4a0d908a9aae62
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 12:01:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ st.title("Mon premier tableau de bord Streamlit")
 #st.table(data)
 #Affichage des 5 premières lignes du jeu de doonnées
 st.write(data.head())
+st.write(count_list = [(df.Species == 'setosa').sum(), (df.Species == 'versicolor').sum(), (df.Species == 'virginica').sum()]
+label_list = list(df['Species'].unique())
+plt.figure(figsize = (10, 7))
+plt.pie(count_list, labels = label_list, autopct = "%.2f %%", startangle = 90, explode = (0.1, 0.1, 0.1), textprops = {'fontsize': 8}, colors=['gold', 'orchid', 'green', "#77BFE2"])
+plt.title('Distribution des Classes', fontsize = 30)
+plt.show())
