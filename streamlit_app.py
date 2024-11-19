239024f2584dab8cf1f4d905152ab1344da0e90b
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 11:31:03 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd 
 # Charger les données
 data = pd.read_csv('Iris.csv', delimiter=";") 
+#Affichage des 5 premières lignes du jeu de doonnées
+df.head()
 #Créer un titre
 st.title("Mon premier tableau de bord Streamlit") 
 #Afficher les données dans un tableau
