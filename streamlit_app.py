9766b03db2bba06223ca3d3c7ee3c68c48b4a73d
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-19 11:38:00 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.title("Mon premier tableau de bord Streamlit")
 #Afficher les données dans un tableau
 st.table(data)
 #Affichage des 5 premières lignes du jeu de doonnées
-st.data.head()
+st.read(data.head())
