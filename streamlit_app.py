98bf5acd641d969dc5851c379e1a1cc7256b693c
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-23 04:41:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import altair as alt
 
 
 # Create two columns
-col1, col2 = st.columns([0.6,0.7],gap="large")  
+col1, col2 = st.columns([0.8,0.7],gap="large")  
 
 # Content for the first column
 with col1:
